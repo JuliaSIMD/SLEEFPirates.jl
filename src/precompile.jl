e0d2e4eee2ca93225b6a26c6c6a57642aac5a55f
Chris Elrod <elrodc@gmail.com>
2019-12-03 16:52:11 -0500

    Removed some precompile statements.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,4 +1,4 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{SLEEFPirates.var"##s13#4",Any,Any,Any,Any,Any})
+    # precompile(Tuple{SLEEFPirates.var"##s13#4",Any,Any,Any,Any,Any})
 end
