e5e81e20c8e061a9f73c9d54858c06fc007da9c6
Oscar Smith <oscardssmith@gmail.com>
2020-08-03 00:52:12 -0400

    myexp->exp

--- src/exp.jl
+++ src/exp.jl
@@ -18,27 +18,27 @@ MIN_EXP(::Val{10}, ::Type{Float32}) = -45.15449934959718f0      # log10 2^-150
 
 # 256/log(base, 2) (For Float64 reductions)
 LogBo256INV(::Val{2}, ::Type{Float64})    = 256.
-LogBo256INV(::Val{ℯ}, ::Type{Float64})     = 369.3299304675746
+LogBo256INV(::Val{ℯ}, ::Type{Float64})    = 369.3299304675746
 LogBo256INV(::Val{10}, ::Type{Float64})   = 850.4135922911647
 # -log(base, 2)/256 in upper and lower bits
 LogBo256U(::Val{2}, ::Type{Float64})      = -0.00390625
-LogBo256U(::Val{ℯ}, ::Type{Float64})       = -0.0027076061740622863
+LogBo256U(::Val{ℯ}, ::Type{Float64})      = -0.0027076061740622863
 LogBo256U(::Val{10}, ::Type{Float64})     = -0.0011758984205624266
-LogBo256L(base::Val{2}, ::Type{Float64})  = 0.
+LogBo256L(base::Val{2}, ::Type{Float64})  =  0.
 LogBo256L(base::Val{ℯ}, ::Type{Float64})  = -9.058776616587108e-20
 LogBo256L(base::Val{10}, ::Type{Float64}) = 1.0952062999160822e-20
 
 # 1/log(base, 2) (For Float32 reductions)
-LogBINV(::Val{2}, ::Type{Float32})    = 1f0
-LogBINV(::Val{ℯ}, ::Type{Float32})    = 1.442695f0
-LogBINV(::Val{10}, ::Type{Float32})   = 3.321928f0
+LogBINV(::Val{2}, ::Type{Float32})    =  1f0
+LogBINV(::Val{ℯ}, ::Type{Float32})    =  1.442695f0
+LogBINV(::Val{10}, ::Type{Float32})   =  3.321928f0
 # -log(base, 2) in upper and lower bits
 LogBU(::Val{2}, ::Type{Float32})      = -1f0
 LogBU(::Val{ℯ}, ::Type{Float32})      = -0.6931472f0
 LogBU(::Val{10}, ::Type{Float32})     = -0.30103f0
 LogBL(base::Val{2}, ::Type{Float32})  =  0f0
-LogBL(base::Val{ℯ}, ::Type{Float32})  = 1.9046542f-9
-LogBL(base::Val{10}, ::Type{Float32}) = 1.4320989f-8
+LogBL(base::Val{ℯ}, ::Type{Float32})  =  1.9046542f-9
+LogBL(base::Val{10}, ::Type{Float32}) =  1.4320989f-8
 
 # Range reduced kernels          
 @inline function expm1b_kernel(::Val{2}, x::Union{Float64, SVec{W, Float64} where W})
@@ -72,20 +72,20 @@ end
 
 const J_TABLE= Float64[2.0^(big(j-1)/256) for j in 1:256]
 
-@noinline function myexp_fallback(::Val{2}, x::SVec{W,T}) where {W, T}
+@noinline function exp_fallback(::Val{2}, x::SVec{W,T}) where {W, T}
     return SVec(ntuple(Val(W)) do i Core.VecElement(exp2(x[i])) end)
 end
-@noinline function myexp_fallback(::Val{ℯ}, x::SVec{W,T}) where {W, T}
+@noinline function exp_fallback(::Val{ℯ}, x::SVec{W,T}) where {W, T}
     return SVec(ntuple(Val(W)) do i Core.VecElement(exp(x[i])) end)
 end
-@noinline function myexp_fallback(::Val{10}, x::SVec{W,T}) where {W, T}
+@noinline function exp_fallback(::Val{10}, x::SVec{W,T}) where {W, T}
     return SVec(ntuple(Val(W)) do i Core.VecElement(exp10(x[i])) end)
 end
 
-for (func, base) in (:myexp2=>Val(2), :myexp=>Val(ℯ), :myexp10=>Val(10))
+for (func, base) in (:exp2=>Val(2), :exp=>Val(ℯ), :exp10=>Val(10))
     @eval begin
         @inline function ($func)(x::SVec{W,T}) where {W, T<:Float64}
-            any(!(abs(x) < MAX_EXP($base, T))) && return myexp_fallback($base, x)
+            any(!(abs(x) < MAX_EXP($base, T))) && return exp_fallback($base, x)
             N_float = vmuladd(x, LogBo256INV($base, T), MAGIC_ROUND_CONST(T))
             N = reinterpret(SVec{W,Int64}, N_float) % Int32
             N_float = vsub(N_float, MAGIC_ROUND_CONST(T))
@@ -101,7 +101,7 @@ for (func, base) in (:myexp2=>Val(2), :myexp=>Val(ℯ), :myexp10=>Val(10))
         end
         
         @inline function ($func)(x::SVec{W,T}) where {W, T<:Float32}
-            any(!(abs(x) < MAX_EXP($base, T))) && return myexp_fallback(x)
+            any(!(abs(x) < MAX_EXP($base, T))) && return exp_fallback(x)
             N_float = vmuladd(x, LogBINV($base, T), MAGIC_ROUND_CONST(T))
             N = reinterpret(SVec{W,Int32}, N_float)
             N_float = vsub(N_float, MAGIC_ROUND_CONST(T))
