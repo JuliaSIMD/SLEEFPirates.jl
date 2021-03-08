49aa2c55db1ba53fab0a02f4b0ec74baa5e1f85b
Chris Elrod <elrodc@gmail.com>
2021-03-08 10:14:34 -0500

    Print some feature info during tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 include("testsetup.jl")
 
+@show Sys.CPU_NAME
+@show VectorizationBase.has_feature(Val(:x86_64_avx))
+@show VectorizationBase.has_feature(Val(:x86_64_avx2))
+@show VectorizationBase.has_feature(Val(:x86_64_fma))
+@show VectorizationBase.has_feature(Val(:x86_64_avx512f))
+@show VectorizationBase.has_feature(Val(:x86_64_avx512vl))
+@show VectorizationBase.has_feature(Val(:x86_64_avx512dq))
+
 function runtests()
     @testset "SLEEFPirates" begin
         # @test isempty(detect_unbound_args(SLEEFPirates))
